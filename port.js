d200857ce0bd212bb2cb8f1d722af3edd211f6cf
Akshay P Johnson <135425200+R0NIN007@users.noreply.github.com>
2025-08-27 11:21:50 +0530

    Add files via upload
    initial commit

---
+++ port.js
@@ -0,0 +1,7 @@
+function readMore() {
+  var expandInfo = document.getElementById("heroo-info-js");
+  var mainHeadings = document.getElementById("heroo-headings-js");
+  
+  mainHeadings.style.transform = "scale(0.7)";
+  expandInfo.style.height = "350px";
+}
